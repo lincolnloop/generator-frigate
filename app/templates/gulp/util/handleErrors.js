b85c811d61ef7daf0f38770db241df83b566c318
Brian Luft <brian@lincolnloop.com>
2014-12-10 12:30:14 -0800

    Removes dead code

--- app/templates/gulp/util/handleErrors.js
+++ app/templates/gulp/util/handleErrors.js
@@ -1,4 +1,5 @@
 var notify = require("gulp-notify");
+var gutil = require("gulp-util");
 var beep = require("./beep");
 
 module.exports = function() {
@@ -6,7 +7,6 @@ module.exports = function() {
   var args = Array.prototype.slice.call(arguments);
 
   // Send error to notification center with gulp-notify
-  beep();
   notify.onError({
     title: "Compile Error",
     message: "<%= error.message %>"
@@ -15,18 +15,3 @@ module.exports = function() {
   // Keep gulp from hanging on this task
   this.emit('end');
 };
-
-/*
-var handleError = function(task) {
-  return function(err) {
-    beep();
-    <% if (systemNotifications) { %>
-      notify.onError({
-        message: task + ' failed, check the logs..',
-        sound: false
-      })(err);
-    <% } %>
-    gutil.log(gutil.colors.bgRed(task + ' error:'), gutil.colors.red(err));
-  };
-};
-*/
