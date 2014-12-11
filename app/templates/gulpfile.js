8614fd0bb053d8ac67bc1642d207eebe3ac63226
Brian Luft <brian@lincolnloop.com>
2014-12-11 11:32:37 -0800

    Add FRIGATE header to output when gulp starts (experimental)

--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -1,8 +1,18 @@
 'use strict';
 var gulp = require('gulp');
 var requireDir = require('require-dir');
+var chalk = require('chalk');
 
 // Require all tasks in gulp/tasks, including subfolders
 requireDir('./gulp/tasks', { recurse: true });
+
+console.log("");
+console.log(chalk.cyan("███████╗██████╗ ██╗ ██████╗  █████╗ ████████╗███████╗"));
+console.log(chalk.cyan("██╔════╝██╔══██╗██║██╔════╝ ██╔══██╗╚══██╔══╝██╔════╝"));
+console.log(chalk.cyan("█████╗  ██████╔╝██║██║  ███╗███████║   ██║   █████╗  "));
+console.log(chalk.cyan("██╔══╝  ██╔══██╗██║██║   ██║██╔══██║   ██║   ██╔══╝  "));
+console.log(chalk.cyan("██║     ██║  ██║██║╚██████╔╝██║  ██║   ██║   ███████╗"));
+console.log(chalk.cyan("╚═╝     ╚═╝  ╚═╝╚═╝ ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚══════╝"));
+console.log("");
 
 gulp.task('default', ['watch']);
