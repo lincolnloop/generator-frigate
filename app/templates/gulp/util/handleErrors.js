68a7321892fe6036c05844adfc9e4f1094232114
Marco Louro <mlouro@gmail.com>
2015-04-28 12:44:29 +0200

    a little more cleanup

--- app/templates/gulp/util/handleErrors.js
+++ app/templates/gulp/util/handleErrors.js
@@ -1,11 +1,9 @@
 var notify = require("gulp-notify");
-var gutil = require("gulp-util");
 
 module.exports = function() {
   var args = Array.prototype.slice.call(arguments);
 
   // Send error to notification center with gulp-notify
-  beep();
   notify.onError({
     title: "Compile Error",
     message: "<%= error.message %>"
