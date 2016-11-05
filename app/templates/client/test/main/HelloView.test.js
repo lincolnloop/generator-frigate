14901022297beff67809de459fb4e04c5da8cbb4
Marco Louro <mlouro@gmail.com>
2016-11-05 11:12:20 +0100

    test tweaks, removed chai

--- app/templates/client/test/main/HelloView.test.js
+++ app/templates/client/test/main/HelloView.test.js
@@ -1,14 +1,14 @@
 "use strict";
-var expect = require('chai').expect;
+import assert from 'assert';
 import {shallow, mount, render} from 'enzyme';
 import React from 'react';
 import HelloView from '../../js/main/HelloView.jsx';
 
-describe('Test for Hello View', function(){
-  describe('render()', function(){
-    it('should have one h1 element', function(){
+describe('Test for Hello View', function() {
+  describe('render()', function() {
+    it('should have one h1 element', function() {
       const wrapper = shallow(<HelloView />);
-      expect(wrapper.find('h1')).to.have.length(1);
+      expect(wrapper.find('h1').length).toEqual(1);
     });
   });
 });
--- app/templates/client/test/main/main.test.js
+++ app/templates/client/test/main/main.test.js
@@ -1,9 +1,9 @@
 "use strict";
-var assert = require("assert");
+import assert from 'assert';
 
-describe('Test for main', function(){
-  describe('#indexOf()', function(){
-    it('should return -1 when the value is not present', function(){
+describe('Test for main', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
       assert.equal(-1, [1,2,3].indexOf(5));
       assert.equal(-1, [1,2,3].indexOf(0));
     });
